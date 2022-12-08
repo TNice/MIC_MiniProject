586cb512beb17e2f35a9345008a7cd90bc5b7263
TNice <tylernicewarner@yahoo.com>
2022-12-08 14:05:47 -0600

    finished plugin classifier

--- src/plugins/PetriNetPlugin/PetriNetPlugin/__init__.py
+++ src/plugins/PetriNetPlugin/PetriNetPlugin/__init__.py
@@ -30,25 +30,29 @@ class PetriNetPlugin(PluginBase):
 
         places = {}
         transitions = {}
+        graph = {}
 
         nodes = core.load_children(active_node)
         
+        #Creates dictionaries of places and transitions with their in and out arcs
         for node in nodes:
             if core.is_type_of(node, META['Place']):
                 places[core.get_path(node)] = {'in': [], 'out': []}
+                graph[core.get_path(node)] = []
             elif core.is_type_of(node, META['Transition']):
                 transitions[core.get_path(node)] = {'in': [], 'out': []}
-
-        
+                graph[core.get_path(node)] = []
 
         for node in nodes:
             if core.is_type_of(node, META['Arc']):
                 if core.get_pointer_path(node, 'src') in places:
                     places[core.get_pointer_path(node, 'src')]['out'].append(core.get_pointer_path(node, 'dst'))
                     transitions[core.get_pointer_path(node, 'dst')]['in'].append(core.get_pointer_path(node, 'src'))
+                    graph[core.get_pointer_path(node, 'src')].append(core.get_pointer_path(node, 'dst'))
                 elif core.get_pointer_path(node, 'src') in transitions:
                     transitions[core.get_pointer_path(node, 'src')]['out'].append(core.get_pointer_path(node, 'dst'))
                     places[core.get_pointer_path(node, 'dst')]['in'].append(core.get_pointer_path(node, 'src'))
+                    graph[core.get_pointer_path(node, 'src')].append(core.get_pointer_path(node, 'dst'))
 
         
 
@@ -77,11 +81,49 @@ class PetriNetPlugin(PluginBase):
             if len(places[p]['in']) != 1 or len(places[p]['out']) != 1:
                 isMarked = False
                 break
+
+        #Check For Source and Sink
+        sources = []
+        sinks = []
+        for p in places:
+            if len(places[p]['in']) == 0:
+                sources.append(p)
+
+            if len(places[p]['out']) == 0:
+                sinks.append(p)
+
         
-        
+
+        if len(sources) == 1 and len(sinks) == 1:
+            isWorkflow = True
+
+        if isWorkflow:
+            #Check Workflow
+            paths = [[]]
+
+            def GetAllPaths(graph, currentNode, dst, path):
+                path.append(currentNode)
+                if currentNode == dst:
+                    paths.append(path)
+                for next in graph[currentNode]:
+                    if next in path:
+                        continue
+                    GetAllPaths(graph, next, dst, path.copy())
+                 
+            GetAllPaths(graph, sources[0], sinks[0], [])
+
+            for p in places:
+                if not isWorkflow:
+                    break
+                for path in paths:
+                    if p in path:
+                        break
+                    isWorkflow = False
+
+        #Create Output Notification
         networkType = {'Free Choice Net': isFreeChoice, 'State Machine': isStateMachine, 'Marked Graph': isMarked, 'Workflow Net': isWorkflow}
         notify_str = "Network is "
-        
+
         i = 0
         for k in networkType:
             if networkType[k] == False:
@@ -94,5 +136,7 @@ class PetriNetPlugin(PluginBase):
 
             i += 1
         
+        if notify_str == "Network is ":
+            notify_str = 'Petri Net Has No Special Classification' 
 
         self.send_notification(notify_str)
